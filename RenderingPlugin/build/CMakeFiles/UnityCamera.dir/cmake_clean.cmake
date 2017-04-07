e47faa26ae11ad88b9d7d9c54ee6a2bc80139648
melights <alwaysunny@gmail.com>
2017-04-08 00:40:13 +0100

    first commit

---
+++ RenderingPlugin/build/CMakeFiles/UnityCamera.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UnityCamera.dir/RenderingPlugin.cpp.o"
+  "CMakeFiles/UnityCamera.dir/VideoCap.cpp.o"
+  "libUnityCamera.pdb"
+  "libUnityCamera.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UnityCamera.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
